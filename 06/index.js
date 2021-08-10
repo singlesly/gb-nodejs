7e8073c55617b86ba0f913a3aad9d94114152d54
Artem Ilinykh <devsinglesly@gmail.com>
2021-08-10 23:43:28 +0600

    socket example

---
+++ 06/index.js
@@ -0,0 +1,26 @@
+const io = require('socket.io');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((request, response) => {
+    const indexPath = path.join(__dirname, "index.html");
+    const readStream = fs.createReadStream(indexPath);
+    readStream.pipe(response);
+});
+
+const socket = io(server);
+
+socket.on('connection', client => {
+    console.log('New client connected');
+
+    client.on('CLIENT_MSG', data => {
+        const payload = {
+            message: data.message.split('').reverse().join('')
+        };
+        client.emit('SERVER_MSG', payload)
+        client.broadcast.emit('SERVER_MSG', payload);
+    });
+});
+
+server.listen(3000);
